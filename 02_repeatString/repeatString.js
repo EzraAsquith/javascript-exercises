a9f4ca0892df8384e072bcd417347d398859b523
Ezra Asquith <Ezra.asquith@gmail.com>
2024-08-08 20:18:53 -0700

    Exercise 2 - Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if(times < 0) { return "ERROR"}
+    let yourString = ``
+    for(let i = 0; i < times; i++) { 
+        yourString += word
+    } 
+    return yourString
 };
 
 // Do not edit below this line
