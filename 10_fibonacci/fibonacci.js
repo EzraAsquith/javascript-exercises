354f009f610d1f5b7f518f4bc416e1fc7f1af540
Ezra Asquith <Ezra.asquith@gmail.com>
2024-08-10 11:20:20 -0700

    Exercise 10 - Done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(final) {
+    if(final < 0){
+        return "OOPS"
+    }
+    if(!Number.isInteger(final)) {
+        final = parseInt(final, 10)
+    }
+    let fib = [0, 1]
+    for(let i = 1; i < final; i++){
+        let temp = fib[i] + fib[i-1]
+        fib.push(temp)
+    }
+    return fib[final]
 };
 
 // Do not edit below this line
