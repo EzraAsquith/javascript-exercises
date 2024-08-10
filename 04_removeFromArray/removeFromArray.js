eccaebdf1a11104acb158b4be0bcba5026be29fa
Ezra Asquith <Ezra.asquith@gmail.com>
2024-08-10 09:22:06 -0700

    Exercise 4 - Done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let cleanedArray = []
+    for(let i = 0; i < array.length; i++){
+        if(!args.includes(array[i])) {
+            cleanedArray.push(array[i])
+        }
+    }
+    return cleanedArray
 };
 
 // Do not edit below this line
