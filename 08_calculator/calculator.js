2adc848043ab4321962f959be415b8516da3fa10
Ezra Asquith <Ezra.asquith@gmail.com>
2024-08-10 10:50:57 -0700

    Exercise 8 - Done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a , b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for(let i=0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let sum = 1;
+  for(let i=0; i < arr.length; i++) {
+    sum *= arr[i]
+  }
+  return sum
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  let result = 1;
+	for(let i=0; i < power; i++) {
+    result *= base
+  }
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(fact) {
+	let result = 1;
+  for(let i = 1; i <= fact; i++) {
+    result *= i
+  }
+  return result
 };
 
 // Do not edit below this line
