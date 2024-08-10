be10bf3e5ef1708d32afb8540bd616ac6cbd44a3
Ezra Asquith <Ezra.asquith@gmail.com>
2024-08-10 11:04:44 -0700

    Exercise 9 - Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase()
+    word = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    word = word.replace(/\s/g, '');
+    let wordReversed = ''
+    for(let i = word.length - 1; i >= 0; i--) {
+        wordReversed += word[i]
+    }
+    return word === wordReversed
 };
 
 // Do not edit below this line
