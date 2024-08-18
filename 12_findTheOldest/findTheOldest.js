7fc4fa8e4ed9d981a2083f2674bbac541a78160a
Ezra Asquith <Ezra.asquith@gmail.com>
2024-08-17 19:30:12 -0700

    Commit

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldestName;
+    let oldestAge = 0;
+    let CURRENT_YEAR = 2024;
+    for(let i = 0; i < arr.length; i++){
+        oldestName = arr[i]["name"]
+    }
+    return oldestName;
 };
 
 // Do not edit below this line
