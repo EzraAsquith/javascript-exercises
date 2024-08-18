d9cd22615a5cbfe97b949fc5d7c6a1c7d9d51508
Ezra Asquith <Ezra.asquith@gmail.com>
2024-08-18 12:07:48 -0700

    Exercise 8 - Done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,31 +7,25 @@ const subtract = function(a, b) {
 };
 
 const sum = function(arr) {
-  let sum = 0;
-  for(let i=0; i < arr.length; i++) {
-    sum += arr[i]
-  }
-  return sum
+  return arr.reduce(add, 0)
 };
 
 const multiply = function(arr) {
-  let sum = 1;
-  for(let i=0; i < arr.length; i++) {
-    sum *= arr[i]
-  }
-  return sum
+  return arr.reduce((total, currentNumber) => {
+    return total * currentNumber;
+  }, 1)
 };
 
 const power = function(base, power) {
-  let result = 1;
-	for(let i=0; i < power; i++) {
-    result *= base
+  let total = 1;
+  for(let i = 0; i < power; i++) { 
+    total *= base
   }
-  return result
+  return total
 };
 
 const factorial = function(fact) {
-	let result = 1;
+  let result = 1;
   for(let i = 1; i <= fact; i++) {
     result *= i
   }
